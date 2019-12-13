233c32d3a5a39e878490baefcc1eb64f87b5feac
Andrey Kharitonov <rampelstillskin@gmail.com>
2019-12-13 19:23:30 +0300

    updated requests

--- src/Components/weatherGenerator/WeatherGenerator.js
+++ src/Components/weatherGenerator/WeatherGenerator.js
@@ -1,82 +1,95 @@
-import React, {Component} from 'react';
-import WeatherBlock from '../weatherBlock/WeatherBlock';
-import '../../index.scss';
+import React, { Component } from "react";
+import WeatherBlock from "../weatherBlock/WeatherBlock";
+import "../../index.scss";
 
-const API_KEY = 'd1663099de2c72e24962a3ac1d663db6';
+const API_KEY = "d1663099de2c72e24962a3ac1d663db6";
 
 class WeatherGenerator extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			"dataWeather": {},
-			"city": '',
-			"nameCity": '',
-			"currentTemperature": '',
-			"minTemperature": '',
-			"maxTemperature": '',
-			"cloudiness": '',
-			"icon": '',
-			"humidity": '',
-			"sunrise": '',
-			"sunset": '',
-			"wind": '',
-			"pressure": '',
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.sunTime = this.sunTime.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataWeather: {},
+      city: "",
+      nameCity: "",
+      currentTemperature: "",
+      minTemperature: "",
+      maxTemperature: "",
+      cloudiness: "",
+      icon: "",
+      humidity: "",
+      sunrise: "",
+      sunset: "",
+      wind: "",
+      pressure: "",
+      days: []
+    };
+  }
 
-	sunTime(time) {
-		const unixSunrise = new Date(time * 1000);
-		const hours = ('0' + unixSunrise.getHours()).slice(-2),
-					minute = ('0' + unixSunrise.getMinutes()).slice(-2);
-		return `${hours}:${minute}`;
-	};
-			
-	handleChange(event) {
-		const {name, value} = event.target;
-		this.setState({ [name]: value })
-	}
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
-	handleSubmit(event) {
-		event.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
 
-		if (this.state.city) {
-			fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&lang=ru&units=metric&appid=${API_KEY}`)
-				.then(response => response.json())
-				.then(data => {
-					this.setState({
-						dataWeather: data,
-						nameCity: data.name,
-						currentTemperature: `${data.main.temp}°`,
-						maxTemperature: `${data.main.temp_max}°`,
-						cloudiness: data.weather[0].description,
-						humidity: `${data.main.humidity}%`,
-						sunrise: this.sunTime(data.sys.sunrise),
-						sunset: this.sunTime(data.sys.sunset),
-						wind: data.wind,
-						pressure: data.main.pressure,
-						icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-					})
-				})
-				.catch(error => console.error(`Error: ${error}`))
-		} else {
-			alert('введите город');
-		}
-	}
+    let myInit = {
+      method: "GET",
+      referrer: "no-referrer",
+      mode: "cors",
+    };
 
-	render() {
-		return(
-			<div className="weather-block__wrapper">
-				<WeatherBlock 
-					data={this.state}
-					handleChange={this.handleChange}
-					handleSubmit={this.handleSubmit}
-				/>
-			</div>
-		)
-	}
+    let requestCurrentDay = fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&lang=ru&units=metric&appid=${API_KEY}`,
+      myInit
+    );
+    let requestFiveDays = fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${this.state.city}&lang=ru&units=metric&appid=${API_KEY}`,
+      myInit
+    );
+
+    if (this.state.city) {
+      Promise.all([requestCurrentDay, requestFiveDays]).then(elements => {
+        return Promise.all(elements.map(response => response.json()))
+          .then(([currentDay, fiveDays, currentIconWeather]) => {
+
+            const fiveDaysData = fiveDays.list.filter(value =>
+              value.dt_txt.includes("15:00:00")
+            );
+
+            this.setState({
+              dataWeather: currentDay,
+              nameCity: currentDay.name,
+              currentTemperature: currentDay.main.temp,
+              maxTemperature: `${currentDay.main.temp_max}°`,
+              cloudiness: currentDay.weather[0].description,
+              humidity: `${currentDay.main.humidity}%`,
+              sunrise: currentDay.sys.sunrise,
+              sunset: currentDay.sys.sunset,
+              wind: currentDay.wind,
+              pressure: currentDay.main.pressure,
+              icon: `http://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png`,
+              days: fiveDaysData
+            });
+          })
+          .catch(error => console.error(`Error: ${error}`));
+      })
+    } else {
+      alert('Введите пожалуйста город')
+    }
+  };
+
+  render() {
+    return (
+      <section className="weather-block__wrapper">
+        <WeatherBlock
+          data={this.state}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+      </section>
+    );
+  }
 }
 
 export default WeatherGenerator;
