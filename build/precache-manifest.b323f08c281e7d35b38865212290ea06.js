702087043edb1c603a28c431f3e281ff4c9c7af7
Andrey Kharitonov <rampelstillskin@gmail.com>
2020-03-02 14:56:32 +0300

    added build

---
+++ build/precache-manifest.b323f08c281e7d35b38865212290ea06.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230689ca939c888509eccc3c5ec33688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4f1058d5d0ce1db9c82",
+    "url": "/static/css/main.ab2fa61f.chunk.css"
+  },
+  {
+    "revision": "3ca50592e570f7dc8240",
+    "url": "/static/js/2.1b8c1991.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1b8c1991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f1058d5d0ce1db9c82",
+    "url": "/static/js/main.55f5bdd0.chunk.js"
+  },
+  {
+    "revision": "c904b5e41f8998ba9d26",
+    "url": "/static/js/runtime-main.e282ae16.js"
+  },
+  {
+    "revision": "6327bf5baf04fcb9418b8997c2e846c6",
+    "url": "/static/media/cloud.6327bf5b.jpg"
+  },
+  {
+    "revision": "20b2334bad1855f39a09463c2636c9c8",
+    "url": "/static/media/day.20b2334b.jpg"
+  },
+  {
+    "revision": "7bfe6423b00a9ddc0aa8ee7a272a83be",
+    "url": "/static/media/down.7bfe6423.svg"
+  },
+  {
+    "revision": "c4b2ded27f31848f768924ebde5e8b83",
+    "url": "/static/media/morning1.c4b2ded2.jpg"
+  },
+  {
+    "revision": "fed4845f58c4d42e66e36f76f5c43b8a",
+    "url": "/static/media/night.fed4845f.jpg"
+  },
+  {
+    "revision": "b25ecc0db952ff866989497d7683a651",
+    "url": "/static/media/rainMorning.b25ecc0d.jpg"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/static/media/search.2ea4c436.svg"
+  },
+  {
+    "revision": "a25d559ae52dd1356385d6b32f066cde",
+    "url": "/static/media/snowStorm.a25d559a.jpg"
+  },
+  {
+    "revision": "141c35076ff0891813d4452851d19c39",
+    "url": "/static/media/stormMorning.141c3507.jpg"
+  },
+  {
+    "revision": "e1182bf4e2ef42353cd348c6e1451e79",
+    "url": "/static/media/up.e1182bf4.svg"
+  }
+]);
